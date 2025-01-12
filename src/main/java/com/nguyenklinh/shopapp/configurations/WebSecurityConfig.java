dd239af7e1373432cdbd99e784d678958ece01a7
nguyenklinh <20010964@st.phenikaa-uni.edu.vn>
2025-01-12 14:44:29 +0700

    feat: JWT feat: Add JWT filter, util and update WebSecurityConfig for authentication

--- src/main/java/com/nguyenklinh/shopapp/configurations/WebSecurityConfig.java
+++ src/main/java/com/nguyenklinh/shopapp/configurations/WebSecurityConfig.java
@@ -2,25 +2,48 @@ package com.nguyenklinh.shopapp.configurations;
 
 import com.nguyenklinh.shopapp.enums.ErrorCode;
 import com.nguyenklinh.shopapp.exceptions.MyException;
+import com.nguyenklinh.shopapp.filters.JwtTokenFilter;
+import com.nguyenklinh.shopapp.models.Role;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import static org.springframework.http.HttpMethod.*;
 
 @Configuration
 @RequiredArgsConstructor
 @EnableMethodSecurity
 public class WebSecurityConfig {
+    private final JwtTokenFilter jwtTokenFilter;
+    @Value("${api.prefix}")
+    private String prefix;
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http){
         try {
             http
                     .csrf(AbstractHttpConfigurer::disable)
-                    .authorizeHttpRequests(reques -> {
-                        reques.requestMatchers("**").permitAll();
+                    .addFilterBefore(jwtTokenFilter, UsernamePasswordAuthenticationFilter.class)
+                    .authorizeHttpRequests(requests -> {
+                        requests
+                                .requestMatchers(
+                                        String.format("%s/users/register",prefix),
+                                        String.format("%s/users/login",prefix)
+                                ).permitAll()
+                                .requestMatchers(PUT,
+                                        String.format("%s/orders/**",prefix)).hasRole(Role.ADMIN)
+                                .requestMatchers(POST,
+                                        String.format("%s/orders/**",prefix)).hasRole(Role.USER)
+                                .requestMatchers(GET,
+                                        String.format("%s/orders/**",prefix)).hasAnyRole(Role.ADMIN, Role.USER)
+                                .requestMatchers(DELETE,
+                                        String.format("%s/orders/**",prefix)).hasRole(Role.ADMIN)
+                                .anyRequest().authenticated();
                     });
 
             return http.build();
---
+++ src/main/java/com/nguyenklinh/shopapp/filters/JwtTokenFilter.java
@@ -0,0 +1,92 @@
+package com.nguyenklinh.shopapp.filters;
+
+import com.nguyenklinh.shopapp.components.JwtTokenUtil;
+import com.nguyenklinh.shopapp.models.User;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.validation.constraints.NotNull;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.data.util.Pair;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.List;
+@Component
+@RequiredArgsConstructor
+public class JwtTokenFilter extends OncePerRequestFilter {
+    @Value("${api.prefix}")
+    private String prefix;
+    private final UserDetailsService userDetailsService;
+    private final JwtTokenUtil jwtTokenUtil;
+    @Override
+    protected void doFilterInternal(@NotNull HttpServletRequest request,
+                                    @NotNull HttpServletResponse response,
+                                    @NotNull FilterChain filterChain) throws ServletException, IOException {
+        try{
+            if(isByPassToken(request)){
+                filterChain.doFilter(request,response);//enable bypass
+                return;
+            }
+            final String authHeader = request.getHeader("Authorization");
+            if(authHeader == null || !authHeader.startsWith("Bearer ")){
+                response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Unauthorized");
+                return;
+            }
+            final String token = authHeader.substring(7);
+            final String phoneNumber = jwtTokenUtil.extractPhoneNumber(token);
+
+            if (phoneNumber != null
+                    && SecurityContextHolder.getContext().getAuthentication() == null){
+
+                User userDetails = (User) userDetailsService.loadUserByUsername(phoneNumber);
+
+                if (jwtTokenUtil.isValidateToken(token,userDetails)){
+                    UsernamePasswordAuthenticationToken authenticationToken =
+                            new UsernamePasswordAuthenticationToken(
+                                    userDetails,
+                                    null,
+                                    userDetails.getAuthorities());
+                    authenticationToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+                    SecurityContextHolder.getContext().setAuthentication(authenticationToken);
+                }
+            }
+            filterChain.doFilter(request,response);//enable bypass
+        }catch (Exception e){
+            response.sendError(HttpServletResponse.SC_UNAUTHORIZED,"Unauthorized");
+        };
+    }
+    private boolean isByPassToken(@NotNull HttpServletRequest request){
+        final List<Pair<String,String>> bypassTokens = Arrays.asList(
+                Pair.of(String.format("%s/roles",prefix), "GET"),
+                Pair.of(String.format("%s/products**",prefix), "GET"),
+                Pair.of(String.format("%s/categories",prefix),"GET"),
+                Pair.of(String.format("%s/users/register",prefix),"POST"),
+                Pair.of(String.format("%s/users/login",prefix),"POST")
+        );
+        String requestPath = request.getServletPath();
+        String requestMethod = request.getMethod();
+
+        for (Pair<String, String> bypassToken : bypassTokens) {
+            //  wildcard `**` to regex `.*`
+            String regexPath = bypassToken.getFirst().replace("**", ".*");
+
+            // Kiểm tra nếu requestPath khớp với regex và method khớp
+            if (requestPath.matches(regexPath) && requestMethod.equalsIgnoreCase(bypassToken.getSecond())) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
